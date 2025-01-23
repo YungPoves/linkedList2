5e6a87743a1498be7ccdde77e287b06569b68fdc
YungPoves <povesr12@gmail.com>
2025-01-23 14:55:38 +0200

    main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+#include "linkedList.h"
 
 int main()
 {
+  Node n1;
+  n1.data = 12345;
   std::cout << "Hello." << std::endl;
+  std::cout << n1.data << std::endl;
 
   return 0;
 }
