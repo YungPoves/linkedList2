cdf7eb5b4864a09e82a01d07a35192c455db046a
YungPoves <povesr12@gmail.com>
2025-01-24 14:50:22 +0200

    src/

--- src/linkedList.h
+++ src/linkedList.h
@@ -1,4 +1,29 @@
-struct Node{
-  int data;
-  Node *next;
+#pragma once
+#include "Node.h"
+
+class LinkedList
+{
+private:
+  Node *head;
+public:
+  LinkedList();
+  ~LinkedList();
+  void PrintList();
 };
+
+LinkedList::LinkedList()
+{
+  head = nullptr;
+}
+
+LinkedList::~LinkedList()
+{
+}
+
+inline void LinkedList::PrintList()
+{
+  if (this->head == nullptr)
+  {
+    std::cout << "List is empty.\n";
+  }  
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
-#include "linkedList.h"
+#include "LinkedList.h"
 
 int main()
 {
+  LinkedList *llist = new LinkedList();
   Node n1;
   n1.data = 12345;
   std::cout << "Hello." << std::endl;
   std::cout << n1.data << std::endl;
+  llist->PrintList();
 
   return 0;
 }
